d77745e3b5ab4510825cb426c1bfc5e5c2681a66
yonghun16 <yonghun16@naver.com>
2023-07-15 13:38:23 +0000

    반응형 HEADER 리사이즈 이벤트

--- Github-html-css/js/main.js
+++ Github-html-css/js/main.js
@@ -8,9 +8,21 @@
     toggleElements();
   });
 
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  });
+
   function toggleElements() {
     [].forEach.call($toggles, function (toggle) {
       toggle.classList.toggle('on');
     });
   }
-})(window, document)
+
+  function offElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.remove('on');
+    });
+  }
+}) (window, document)
